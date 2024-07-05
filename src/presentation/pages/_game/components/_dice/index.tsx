033c63c4566ca4a2200d20f655c901588a933d48
Lucas Rezende <lucas.s.oliveira.rezende@gmail.com>
2024-07-05 15:27:45 -0300

    feat: adds d10 and d12 dices

--- src/presentation/pages/_game/components/_dice/index.tsx
+++ src/presentation/pages/_game/components/_dice/index.tsx
@@ -1,5 +1,8 @@
 import { FC } from 'react';
 
+import { Color } from '@domain/enums';
+import { Vector } from '@domain/utils';
+
 import { useDice } from '@presentation/hooks';
 import { darken, getColor, lighten } from '@presentation/styles/mixins';
 
@@ -10,6 +13,10 @@ import { useMapContext } from '..';
 const d4Edge = 40;
 const d6Edge = 20;
 const d8Edge = 20;
+const d10Edge = 20;
+const d12Edge = 20;
+
+const turn = 2 * Math.PI;
 
 export const Dice: FC<DiceProps> = ({
   value,
@@ -32,6 +39,7 @@ export const Dice: FC<DiceProps> = ({
         opacity={(transparent ? 0.5 : 1) * (overloaded ? 0.5 : 1)}
         style={{ cursor: onClick ? 'pointer' : 'default' }}
         onClick={onClick}
+        strokeLinejoin='round'
       >
         <path
           d={`M${position.x},${position.y} m-${d4Edge / 2},${
@@ -104,6 +112,7 @@ export const Dice: FC<DiceProps> = ({
         opacity={(transparent ? 0.5 : 1) * (overloaded ? 0.5 : 1)}
         onClick={onClick}
         style={{ cursor: onClick ? 'pointer' : 'default' }}
+        strokeLinejoin='round'
       >
         <path
           d={`M${position.x},${position.y} m0,${d6Edge} l${
@@ -174,6 +183,7 @@ export const Dice: FC<DiceProps> = ({
         opacity={(transparent ? 0.5 : 1) * (overloaded ? 0.5 : 1)}
         onClick={onClick}
         style={{ cursor: onClick ? 'pointer' : 'default' }}
+        strokeLinejoin='round'
       >
         <path
           d={`M${position.x},${position.y} m0,${d8Edge} l${
@@ -226,6 +236,16 @@ export const Dice: FC<DiceProps> = ({
           stroke={transparent ? getColor(color) : undefined}
           fill='none'
         />
+        <path
+          d={`M${position.x},${position.y} m0,${d8Edge} l${
+            (d8Edge * Math.sqrt(3)) / 2
+          },-${d8Edge / 2} v-${d8Edge} l-${(d8Edge * Math.sqrt(3)) / 2},-${
+            d8Edge / 2
+          } l-${(d8Edge * Math.sqrt(3)) / 2},${d8Edge / 2} v${d8Edge} z`}
+          stroke={transparent ? getColor(color) : undefined}
+          fill='none'
+        />
+
         {value && !transparent && (
           <text
             style={{
@@ -234,7 +254,281 @@ export const Dice: FC<DiceProps> = ({
               fill: 'white',
             }}
             x={position.x}
-            y={position.y}
+            y={position.y + 2}
+          >
+            {value}
+          </text>
+        )}
+      </g>
+    );
+
+  if (sides === 10)
+    return (
+      <g
+        opacity={(transparent ? 0.5 : 1) * (overloaded ? 0.5 : 1)}
+        onClick={onClick}
+        style={{ cursor: onClick ? 'pointer' : 'default' }}
+        strokeLinejoin='round'
+      >
+        <path
+          d={`M${position.x},${position.y} m0,${d10Edge} l${d10Edge},-${
+            (d10Edge * 2) / 3
+          } v-${(d10Edge * 2) / 3} l-${d10Edge},-${
+            (d10Edge * 2) / 3
+          } l-${d10Edge},${(d10Edge * 2) / 3} v${(d10Edge * 2) / 3} z`}
+          stroke={transparent ? getColor(color) : undefined}
+          fill={transparent ? 'none' : getColor(color)}
+        />
+        <path
+          d={`M${position.x},${position.y} m-${(d10Edge * 2) / 3},${
+            d10Edge / 3
+          } l${(d10Edge * 2) / 3},-${(d10Edge * 4) / 3} l${(d10Edge * 2) / 3},${
+            (d10Edge * 4) / 3
+          }  l-${(d10Edge * 2) / 3},${d10Edge / 3} z`}
+          stroke='none'
+          fill={getColor(color)}
+          opacity={transparent ? 0.25 : 0}
+        />
+        <path
+          d={`M${position.x},${position.y} m-${(d10Edge * 2) / 3},${
+            d10Edge / 3
+          } h-${d10Edge / 3} v-${(d10Edge * 2) / 3} l${d10Edge},-${
+            (d10Edge * 2) / 3
+          } z`}
+          stroke='none'
+          fill={transparent ? 'none' : lighten(getColor(color), 0.2)}
+        />
+        <path
+          d={`M${position.x},${position.y} m${(d10Edge * 2) / 3},${
+            d10Edge / 3
+          } h${d10Edge / 3} v-${(d10Edge * 2) / 3} l-${d10Edge},-${
+            (d10Edge * 2) / 3
+          } z`}
+          opacity={transparent ? 0.5 : 1}
+          stroke='none'
+          fill={transparent ? getColor(color) : darken(getColor(color), 0.05)}
+        />
+        <path
+          d={`M${position.x},${position.y} m-${(d10Edge * 2) / 3},${
+            d10Edge / 3
+          } h-${d10Edge / 3} l${d10Edge},${(d10Edge * 2) / 3} l${d10Edge},-${
+            (d10Edge * 2) / 3
+          } h-${d10Edge / 3} l-${(d10Edge * 2) / 3},${d10Edge / 3} z`}
+          stroke='none'
+          fill={transparent ? getColor(color) : darken(getColor(color), 0.1)}
+        />
+
+        <path
+          d={`M${position.x},${position.y} m-${d10Edge},${d10Edge / 3} h${
+            d10Edge / 3
+          } l${(d10Edge * 2) / 3},-${(d10Edge * 4) / 3}`}
+          stroke={transparent ? getColor(color) : undefined}
+          fill='none'
+        />
+        <path
+          d={`M${position.x},${position.y} m${d10Edge},${d10Edge / 3} h-${
+            d10Edge / 3
+          } l-${(d10Edge * 2) / 3},-${(d10Edge * 4) / 3}`}
+          stroke={transparent ? getColor(color) : undefined}
+          fill='none'
+        />
+        <path
+          d={`M${position.x},${position.y} m0,${d10Edge} v-${d10Edge / 3} l-${
+            (d10Edge * 2) / 3
+          },-${d10Edge / 3}`}
+          stroke={transparent ? getColor(color) : undefined}
+          fill='none'
+        />
+        <path
+          d={`M${position.x},${position.y} m0,${d10Edge} m0,-${d10Edge / 3} l${
+            (d10Edge * 2) / 3
+          },-${d10Edge / 3}`}
+          stroke={transparent ? getColor(color) : undefined}
+          fill='none'
+        />
+
+        {value && !transparent && (
+          <text
+            style={{
+              textAnchor: 'middle',
+              alignmentBaseline: 'middle',
+              fill: 'white',
+            }}
+            x={position.x}
+            y={position.y + 2}
+          >
+            {value}
+          </text>
+        )}
+      </g>
+    );
+
+  if (sides === 12)
+    return (
+      <g
+        opacity={(transparent ? 0.5 : 1) * (overloaded ? 0.5 : 1)}
+        onClick={onClick}
+        style={{ cursor: onClick ? 'pointer' : 'default' }}
+        strokeLinejoin='round'
+      >
+        <path
+          d={`${Array.from({
+            length: 10,
+          })
+            .map(
+              (_, i) =>
+                `${i ? 'L' : 'M'}${
+                  position.x + Math.cos((turn * (i + 0.5)) / 10) * d12Edge
+                },${position.y + Math.sin((turn * (i + 0.5)) / 10) * d12Edge}`,
+            )
+            .join(' ')} z`}
+          fill={transparent ? 'none' : getColor(color)}
+          stroke={transparent ? getColor(color) : undefined}
+          strokeWidth={3}
+        />
+        <path
+          d={`${Array.from({ length: 5 })
+            .map(
+              (_, i) =>
+                `${i ? 'L' : 'M'}${
+                  position.x +
+                  Math.cos((turn * (i - 0.25)) / 5) * ((d12Edge * 2) / 3)
+                },${
+                  position.y +
+                  Math.sin((turn * (i - 0.25)) / 5) * ((d12Edge * 2) / 3)
+                }`,
+            )
+            .join(' ')} z`}
+          fill={transparent ? getColor(color) : 'none'}
+          opacity={transparent ? 0.25 : 1}
+        />
+        <path
+          d={`M${
+            position.x + Math.cos((turn * 2.75) / 5) * ((d12Edge * 2) / 3)
+          },${
+            position.y + Math.sin((turn * 2.75) / 5) * ((d12Edge * 2) / 3)
+          } L${position.x + Math.cos((turn * 2.75) / 5) * d12Edge},${
+            position.y + Math.sin((turn * 2.75) / 5) * d12Edge
+          } L${position.x + Math.cos((turn * 3.25) / 5) * d12Edge},${
+            position.y + Math.sin((turn * 3.25) / 5) * d12Edge
+          } L${position.x + Math.cos((turn * 3.75) / 5) * d12Edge},${
+            position.y + Math.sin((turn * 3.75) / 5) * d12Edge
+          } L${
+            position.x + Math.cos((turn * 3.75) / 5) * ((d12Edge * 2) / 3)
+          },${position.y + Math.sin((turn * 3.75) / 5) * ((d12Edge * 2) / 3)}`}
+          fill={transparent ? 'none' : lighten(getColor(color), 0.2)}
+        />
+        <path
+          d={`${Array.from({ length: 2 })
+            .map(
+              (_, i) =>
+                `M${
+                  position.x +
+                  Math.cos((turn * (i + 1 - 0.25)) / 5) * ((d12Edge * 2) / 3)
+                },${
+                  position.y +
+                  Math.sin((turn * (i + 1 - 0.25)) / 5) * ((d12Edge * 2) / 3)
+                } L${
+                  position.x + Math.cos((turn * (i + 1 - 0.25)) / 5) * d12Edge
+                },${
+                  position.y + Math.sin((turn * (i + 1 - 0.25)) / 5) * d12Edge
+                } L${
+                  position.x + Math.cos((turn * (i + 1 + 0.25)) / 5) * d12Edge
+                },${
+                  position.y + Math.sin((turn * (i + 1 + 0.25)) / 5) * d12Edge
+                } L${
+                  position.x + Math.cos((turn * (i + 1 + 0.75)) / 5) * d12Edge
+                },${
+                  position.y + Math.sin((turn * (i + 1 + 0.75)) / 5) * d12Edge
+                } L${
+                  position.x +
+                  Math.cos((turn * (i + 1 + 0.75)) / 5) * ((d12Edge * 2) / 3)
+                },${
+                  position.y +
+                  Math.sin((turn * (i + 1 + 0.75)) / 5) * ((d12Edge * 2) / 3)
+                }`,
+            )
+            .join(' ')}`}
+          fill={transparent ? getColor(color) : darken(getColor(color), 0.1)}
+        />
+        <path
+          d={`${Array.from({ length: 2 })
+            .map(
+              (_, i) =>
+                `M${
+                  position.x +
+                  Math.cos((turn * (i - 1 - 0.25)) / 5) * ((d12Edge * 2) / 3)
+                },${
+                  position.y +
+                  Math.sin((turn * (i - 1 - 0.25)) / 5) * ((d12Edge * 2) / 3)
+                } L${
+                  position.x + Math.cos((turn * (i - 1 - 0.25)) / 5) * d12Edge
+                },${
+                  position.y + Math.sin((turn * (i - 1 - 0.25)) / 5) * d12Edge
+                } L${
+                  position.x + Math.cos((turn * (i - 1 + 0.25)) / 5) * d12Edge
+                },${
+                  position.y + Math.sin((turn * (i - 1 + 0.25)) / 5) * d12Edge
+                } L${
+                  position.x + Math.cos((turn * (i - 1 + 0.75)) / 5) * d12Edge
+                },${
+                  position.y + Math.sin((turn * (i - 1 + 0.75)) / 5) * d12Edge
+                } L${
+                  position.x +
+                  Math.cos((turn * (i - 1 + 0.75)) / 5) * ((d12Edge * 2) / 3)
+                },${
+                  position.y +
+                  Math.sin((turn * (i - 1 + 0.75)) / 5) * ((d12Edge * 2) / 3)
+                }`,
+            )
+            .join(' ')}`}
+          fill={transparent ? getColor(color) : darken(getColor(color), 0.05)}
+          opacity={transparent ? 0.5 : 1}
+        />
+        <path
+          d={`${Array.from({ length: 5 })
+            .map(
+              (_, i) =>
+                `${i ? 'L' : 'M'}${
+                  position.x +
+                  Math.cos((turn * (i - 0.25)) / 5) * ((d12Edge * 2) / 3)
+                },${
+                  position.y +
+                  Math.sin((turn * (i - 0.25)) / 5) * ((d12Edge * 2) / 3)
+                }`,
+            )
+            .join(' ')} z`}
+          stroke={transparent ? getColor(color) : undefined}
+          fill='none'
+        />
+        <path
+          d={`${Array.from({ length: 5 })
+            .map(
+              (_, i) =>
+                `M${
+                  position.x +
+                  Math.cos((turn * (i - 0.25)) / 5) * ((d12Edge * 2) / 3)
+                },${
+                  position.y +
+                  Math.sin((turn * (i - 0.25)) / 5) * ((d12Edge * 2) / 3)
+                } L${
+                  position.x + Math.cos((turn * (i - 0.25)) / 5) * d12Edge
+                },${position.y + Math.sin((turn * (i - 0.25)) / 5) * d12Edge}`,
+            )
+            .join(' ')}`}
+          stroke={transparent ? getColor(color) : undefined}
+          fill='none'
+        />
+
+        {value && !transparent && (
+          <text
+            style={{
+              textAnchor: 'middle',
+              alignmentBaseline: 'middle',
+              fill: 'white',
+            }}
+            x={position.x}
+            y={position.y + 2}
           >
             {value}
           </text>
@@ -255,6 +549,142 @@ export const Dices: FC<DicesProps> = ({ transparent, hidden }) => {
 
         return <Dice key={dice.id} transparent={transparent} {...dice} />;
       })}
+
+      {/* <Dice
+        id='0'
+        color={Color.GREEN}
+        order={0}
+        value={3}
+        overloadCount={0}
+        overloaded={false}
+        ownerID='0'
+        position={new Vector([0, 1])}
+        sides={4}
+        createdAt={new Date()}
+        updatedAt={new Date()}
+      />
+      <Dice
+        id='0'
+        color={Color.GREEN}
+        order={0}
+        value={3}
+        overloadCount={0}
+        overloaded={false}
+        ownerID='0'
+        position={new Vector([1, 1])}
+        sides={4}
+        createdAt={new Date()}
+        updatedAt={new Date()}
+        transparent
+      />
+      <Dice
+        id='0'
+        color={Color.GREEN}
+        order={0}
+        value={3}
+        overloadCount={0}
+        overloaded={false}
+        ownerID='0'
+        position={new Vector([0, 2])}
+        sides={6}
+        createdAt={new Date()}
+        updatedAt={new Date()}
+      />
+      <Dice
+        id='0'
+        color={Color.GREEN}
+        order={0}
+        value={3}
+        overloadCount={0}
+        overloaded={false}
+        ownerID='0'
+        position={new Vector([1, 2])}
+        sides={6}
+        createdAt={new Date()}
+        updatedAt={new Date()}
+        transparent
+      />
+      <Dice
+        id='0'
+        color={Color.GREEN}
+        order={0}
+        value={3}
+        overloadCount={0}
+        overloaded={false}
+        ownerID='0'
+        position={new Vector([0, 3])}
+        sides={8}
+        createdAt={new Date()}
+        updatedAt={new Date()}
+      />
+      <Dice
+        id='0'
+        color={Color.GREEN}
+        order={0}
+        value={3}
+        overloadCount={0}
+        overloaded={false}
+        ownerID='0'
+        position={new Vector([1, 3])}
+        sides={8}
+        createdAt={new Date()}
+        updatedAt={new Date()}
+        transparent
+      />
+      <Dice
+        id='0'
+        color={Color.GREEN}
+        order={0}
+        value={3}
+        overloadCount={0}
+        overloaded={false}
+        ownerID='0'
+        position={new Vector([0, 4])}
+        sides={10}
+        createdAt={new Date()}
+        updatedAt={new Date()}
+      />
+      <Dice
+        id='0'
+        color={Color.GREEN}
+        order={0}
+        value={3}
+        overloadCount={0}
+        overloaded={false}
+        ownerID='0'
+        position={new Vector([1, 4])}
+        sides={10}
+        createdAt={new Date()}
+        updatedAt={new Date()}
+        transparent
+      />
+      <Dice
+        id='0'
+        color={Color.GREEN}
+        order={0}
+        value={3}
+        overloadCount={0}
+        overloaded={false}
+        ownerID='0'
+        position={new Vector([0, 5])}
+        sides={12}
+        createdAt={new Date()}
+        updatedAt={new Date()}
+      />
+      <Dice
+        id='0'
+        color={Color.GREEN}
+        order={0}
+        value={3}
+        overloadCount={0}
+        overloaded={false}
+        ownerID='0'
+        position={new Vector([1, 5])}
+        sides={12}
+        createdAt={new Date()}
+        updatedAt={new Date()}
+        transparent
+      /> */}
     </>
   );
 };
--- src/presentation/pages/_game/components/_round/index.tsx
+++ src/presentation/pages/_game/components/_round/index.tsx
@@ -15,101 +15,102 @@ import { getColor } from '@presentation/styles/mixins';
 import { useMapContext } from '../_map';
 
 export const Round: FC = () => {
-  const [s, set] = useStates({
-    active: null as string | null,
-  });
+  return null;
+  // const [s, set] = useStates({
+  //   active: null as string | null,
+  // });
 
-  const { navigateToPlayer } = useNavigate();
+  // const { navigateToPlayer } = useNavigate();
 
-  const { players, currentPlayer } = usePlayer();
-  const { round } = useRound();
-  const { currentGame } = useGame();
+  // const { players, currentPlayer } = usePlayer();
+  // const { round } = useRound();
+  // const { currentGame } = useGame();
 
-  const { width } = useMapContext();
+  // const { width } = useMapContext();
 
-  const r = 40;
-  const position = new Vector([width, 0]).sum(-(32 + r), 32 + r);
-  const clockwise = round?.clockwise === 'clockwise' ? 1 : 0;
+  // const r = 40;
+  // const position = new Vector([width, 0]).sum(-(32 + r), 32 + r);
+  // const clockwise = round?.clockwise === 'clockwise' ? 1 : 0;
 
-  const currentI = currentPlayer?.order ?? 0;
+  // const currentI = currentPlayer?.order ?? 0;
 
-  const arcStart = position.sum(
-    new Vector([
-      Math.cos(((2 * Math.PI) / players.length) * currentI),
-      Math.sin(((2 * Math.PI) / players.length) * currentI),
-    ]).mult(r),
-  );
-  const arcEnd = position.sum(
-    new Vector([
-      Math.cos(
-        ((2 * Math.PI) / players.length) *
-          (currentI + (clockwise || -1) * (currentGame?.stateProgress || 1)),
-      ),
-      Math.sin(
-        ((2 * Math.PI) / players.length) *
-          (currentI + (clockwise || -1) * (currentGame?.stateProgress || 1)),
-      ),
-    ]).mult(r),
-  );
+  // const arcStart = position.sum(
+  //   new Vector([
+  //     Math.cos(((2 * Math.PI) / players.length) * currentI),
+  //     Math.sin(((2 * Math.PI) / players.length) * currentI),
+  //   ]).mult(r),
+  // );
+  // const arcEnd = position.sum(
+  //   new Vector([
+  //     Math.cos(
+  //       ((2 * Math.PI) / players.length) *
+  //         (currentI + (clockwise || -1) * (currentGame?.stateProgress || 1)),
+  //     ),
+  //     Math.sin(
+  //       ((2 * Math.PI) / players.length) *
+  //         (currentI + (clockwise || -1) * (currentGame?.stateProgress || 1)),
+  //     ),
+  //   ]).mult(r),
+  // );
 
-  return (
-    <>
-      <circle
-        cx={position.x}
-        cy={position.y}
-        r={r}
-        stroke={getColor()}
-        opacity={0.5}
-        strokeWidth={1}
-        fill='none'
-        strokeDasharray='3'
-      />
+  // return (
+  //   <>
+  //     <circle
+  //       cx={position.x}
+  //       cy={position.y}
+  //       r={r}
+  //       stroke={getColor()}
+  //       opacity={0.5}
+  //       strokeWidth={1}
+  //       fill='none'
+  //       strokeDasharray='3'
+  //     />
 
-      <path
-        d={`M ${arcStart.x} ${arcStart.y} A ${r} ${r} 0 0 ${clockwise} ${arcEnd.x} ${arcEnd.y}`}
-        stroke={getColor(currentPlayer?.color)}
-        strokeWidth={2}
-        fill='none'
-        // style={{
-        //   transform: `rotate(${(360 / players.length) * 1}deg)`,
-        // }}
-      />
+  //     <path
+  //       d={`M ${arcStart.x} ${arcStart.y} A ${r} ${r} 0 0 ${clockwise} ${arcEnd.x} ${arcEnd.y}`}
+  //       stroke={getColor(currentPlayer?.color)}
+  //       strokeWidth={2}
+  //       fill='none'
+  //       // style={{
+  //       //   transform: `rotate(${(360 / players.length) * 1}deg)`,
+  //       // }}
+  //     />
 
-      {players.map((player, i) => (
-        <Transition.Scale
-          key={player.id}
-          active={s.active === player.id}
-          activeFactor={player.id === currentPlayer?.id ? 1.1 : 1.3}
-          ms={100}
-        >
-          <Text
-            // style
-            className='handwriting'
-            textAnchor='middle'
-            alignmentBaseline='middle'
-            fill={player.avatar}
-            stroke='white'
-            cursor='pointer'
-            strokeWidth={3}
-            style={{
-              transform: `translate(${
-                Math.cos((2 * Math.PI) / (i + 1)) * r
-              }px, ${Math.sin((2 * Math.PI) / (i + 1)) * r}px)`,
-              fontSize: player.id === currentPlayer?.id ? '2rem' : undefined,
-            }}
-            // params
-            x={position.x}
-            y={position.y}
-            // handle
-            onMouseEnter={set('active', player.id)}
-            onMouseOut={set('active', null)}
-            onMouseLeave={set('active', null)}
-            onClick={() => navigateToPlayer(player.id)}
-          >
-            {player.avatar}
-          </Text>
-        </Transition.Scale>
-      ))}
-    </>
-  );
+  //     {players.map((player, i) => (
+  //       <Transition.Scale
+  //         key={player.id}
+  //         active={s.active === player.id}
+  //         activeFactor={player.id === currentPlayer?.id ? 1.1 : 1.3}
+  //         ms={100}
+  //       >
+  //         <Text
+  //           // style
+  //           className='handwriting'
+  //           textAnchor='middle'
+  //           alignmentBaseline='middle'
+  //           fill={player.avatar}
+  //           stroke='white'
+  //           cursor='pointer'
+  //           strokeWidth={3}
+  //           style={{
+  //             transform: `translate(${
+  //               Math.cos((2 * Math.PI) / (i + 1)) * r
+  //             }px, ${Math.sin((2 * Math.PI) / (i + 1)) * r}px)`,
+  //             fontSize: player.id === currentPlayer?.id ? '2rem' : undefined,
+  //           }}
+  //           // params
+  //           x={position.x}
+  //           y={position.y}
+  //           // handle
+  //           onMouseEnter={set('active', player.id)}
+  //           onMouseOut={set('active', null)}
+  //           onMouseLeave={set('active', null)}
+  //           onClick={() => navigateToPlayer(player.id)}
+  //         >
+  //           {player.avatar}
+  //         </Text>
+  //       </Transition.Scale>
+  //     ))}
+  //   </>
+  // );
 };
